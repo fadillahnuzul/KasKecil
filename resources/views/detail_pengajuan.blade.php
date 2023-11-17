5f70f44a8d718abf5c41721f8deb8f5520b8450e
nuzul <nuzulnf1712@gmail.com>
2023-11-17 16:49:31 +0700

    fjwklefj

--- resources/views/detail_pengajuan.blade.php
+++ resources/views/detail_pengajuan.blade.php
@@ -171,7 +171,7 @@
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
                                             <div class="text-s font-weight-bold text-success text-uppercase mb-1">
-                                                Transaksi Di Luar Periode</div>
+                                                Transaksi Belum Klaim Luar Periode</div>
                                             <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. {{number_format($transaksiLuarTanggal, 2, ",", ".")}}</div>
                                         </div>
                                         <div class="col-auto">
